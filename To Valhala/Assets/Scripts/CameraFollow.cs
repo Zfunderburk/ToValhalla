c06fcca6d44fa715fe4cfa283887c5e8aa25ef30
Zach Funderburk <zf547414@ohio.edu>
2017-03-23 13:22:26 -0400

    CamerFollow start
    Camera will follow character to extent, than stop moving

--- To Valhala/Assets/Scripts/CameraFollow.cs
+++ To Valhala/Assets/Scripts/CameraFollow.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject player;
+	public GameObject boundsLeft;
+	public GameObject boundsRight;
+	public float yOffset = 1;
+
+
+	void Update ()
+	{
+		float camX = transform.position.x;
+		Vector3 campos = transform.position;
+		if(camX < boundsRight.transform.position.x && camX > boundsLeft.transform.position.x)
+		{
+			campos.x = player.transform.position.x;
+		}
+		campos.y = player.transform.position.y + yOffset;
+		transform.position = campos;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+
 }
